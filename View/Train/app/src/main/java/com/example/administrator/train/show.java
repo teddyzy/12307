8f819ee325a82b3f0840f58d77d267c3cd161de4
teddyzy <1012631645@qq.com>
2017-04-26 14:47:30 +0800

    Add files via upload

---
+++ View/Train/app/src/main/java/com/example/administrator/train/show.java
@@ -0,0 +1,21 @@
+package com.example.administrator.a12307;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] data={"G102 上海虹桥->北京南","G104 上海虹桥->北京南","G6 上海虹桥->北京南"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( MainActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView listview = (ListView) findViewById(R.id.list_view);
+        listview.setAdapter(adapter);
+    }
+
+}
